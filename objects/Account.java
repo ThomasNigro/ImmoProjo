ebd0f32962cd97f9b3f24fd61f1044c322fc530b
Thomas Nigro <tn@thomasnigro.fr>
2014-10-17 16:22:20 +0200

    Added basic Account class

---
+++ objects/Account.java
@@ -0,0 +1,37 @@
+package objects;
+
+public class Account 
+{
+	private int id;
+	private String name;
+	private String login;
+	private String password;
+	
+	public String getName()
+	{
+		return name;
+	}
+
+	public String getLogin() {
+		return login;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+	
+	public Account(String _name, String _login, String _password)
+	{
+		name = _name;
+		login = _login;
+		password = _password;
+	}
+	
+	public static void CreateAccount(Account newAccount)
+	{
+		// Add to DB
+		// Create cookies and stuff
+	}
+	
+	
+}
